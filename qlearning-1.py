c4968846842b6d78b914c361a43781f5b3f4418e
Fred Reiss <reissfc@gmail.com>
2021-03-13 20:52:34 -0500

    update

--- qlearning-1.py
+++ qlearning-1.py
@@ -8,6 +8,9 @@ print(env.observation_space.low)
 print(env.action_space.n)
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+
+print(discrete_os_win_size)
 
 done = False
 
